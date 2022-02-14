958cd267391c313dff8f0c36104204b4fcea822c
dlemburg <daniel.lemburg1@gmail.com>
2022-02-13 23:02:21 -0800

    adding i18n

---
+++ i18n/index.ts
@@ -0,0 +1,3 @@
+const getI18n = (locale) => require(locale)?.default;
+
+export default getI18n;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'es-ES'],
+    defaultLocale: 'en-US',
+  },
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   async redirects() {
     return [
