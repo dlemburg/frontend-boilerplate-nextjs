77c71217eb33c2a9a95a796e9584efadea5a9524
dlemburg <daniel.lemburg1@gmail.com>
2022-02-19 21:03:54 -0800

    changing up config

--- config/index.ts
+++ config/index.ts
@@ -2,13 +2,13 @@ import merge from 'lodash/merge';
 import commonConfig from './common';
 
 const ENV_MAP = {
-  local: 'local',
+  dev: 'dev',
   qa: 'qa',
   uat: 'uat',
   production: 'production',
 };
 
-const LOCAL_DEV_ENVIRONMENTS = [ENV_MAP.local, ENV_MAP.qa, ENV_MAP.uat];
+const LOCAL_DEV_ENVIRONMENTS = [ENV_MAP.dev, ENV_MAP.qa, ENV_MAP.uat];
 const env = process.env.NODE_ENV ?? '';
 
 export const isLocalDev = () => LOCAL_DEV_ENVIRONMENTS.includes(env);
@@ -16,7 +16,7 @@ export const isProduction = () => [ENV_MAP.production].includes(env);
 export const isDevelop = () => [ENV_MAP.qa].includes(env);
 
 const getConfig = () => {
-  const environment = isLocalDev() ? ENV_MAP.local : env;
+  const environment = isLocalDev() ? ENV_MAP.dev : env;
   const envConfig = require(`./${environment}`).default;
 
   const merged = merge(envConfig, commonConfig);
